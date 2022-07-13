eeddb343c685a3bb91f8c9842314b057f8b1330b
biplab5464 <bmg321111@outlook.com>
2022-07-13 23:48:46 +0530

    introducing hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 //use std::io;
-use store::shop;
+use store::shop; 
+use std::collections::HashMap;
 
 fn main() {
 
     // let mut fruit = String::new();
     // let mut num_str = String::new();
+    let mut arr = HashMap::new(); 
 
     // println!("Enter the fruit name :");
     // io::stdin().read_line(&mut fruit).unwrap();
@@ -14,9 +16,16 @@ fn main() {
     // let num : i32 = num_str.trim().parse().unwrap();
     let mut total : i32 = 0;
 
-    let arr = [("apple", 35),("banana",45),("mango",34)];
-    for ele in arr{
-        let mut bill = shop::Bill::new_bill(ele.0.to_string(),ele.1,0);
+
+
+    
+    arr.entry(String::from("apple")).or_insert(12);
+    arr.entry(String::from("banana")).or_insert(35);
+    arr.entry(String::from("mango")).or_insert(6);
+    
+
+    for (key,value) in &arr{
+        let mut bill = shop::Bill::new_bill(key.to_string().clone(),*value,0);
         //bill.print_bill();
         let result = bill.get_bill();
         total = total + result.0;
